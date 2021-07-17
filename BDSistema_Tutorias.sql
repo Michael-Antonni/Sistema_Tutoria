168ede04e46d8636b402049817cc981ce74fda16
Michael-Antonni <72481616+Michael-Antonni@users.noreply.github.com>
2021-07-17 10:30:01 -0500

    se modifica todas las tablas
    se uo la estrcutura de la docente

--- BDSistema_Tutorias.sql
+++ BDSistema_Tutorias.sql
@@ -14,10 +14,43 @@ go
 	APaterno varchar(15) not null,
 	AMaterno varchar(15) not null,
 	Nombres varchar(10)not null,
-	CarreraP varchar(25)not null,
+	CodigoEP varchar(4)not null,
+		--Primary key and foreign keys
+	primary key (CodigoD),
+	foreign key(CodigoEP) references Escuela_Profesional
+ );
+ create table TDocente_Contratado
+ (	
+	--Atributos
+	CodigoD varchar(7) not null,
 	Categoria varchar(2) not null,
+	Horas int,
+	Clasificacion varchar(5) not null,
 	--Primary key and foreign keys
-	primary key (CodigoD)
+	primary key (CodigoD),
+	foreign key (CodigoD) references TDocente
+
+ );
+ drop table TDocente_Nombrado
+ create table TDocente_Nombrado
+ (
+	CodigoD varchar(7) not null,
+	Tipo varchar(10) not null,
+	--Primary key and foreign keys
+	primary key (CodigoD),
+	foreign key (CodigoD) references TDocente,
+	foreign key (Tipo) references TDocente_Nombrado_Tipo
+ );
+ drop table TDocente_Nombrado_Tipo
+create table TDocente_Nombrado_Tipo
+ (
+	
+	Tipo varchar(10) not null,
+	Caracteristica varchar(10) not null,
+	--Primary key and foreign keys
+	primary key (Tipo),
+	
+
  );
 
  create table Escuela_Profesional
@@ -36,6 +69,7 @@ go
 	AMaterno varchar(15) not null,
 	Nombres varchar(10)not null,
 	CodigoEP varchar(4)not null,
+	Estado varchar(40),
 	--Primary key and foreign keys
 	primary key (CodigoE),
 	foreign key(CodigoEP) references Escuela_Profesional
@@ -50,3 +84,6 @@ go
 	foreign key (CodigoE) references TEstudiante,
 	foreign key (CodigoD) references TDocente
  );
+
+ --===============================================================
+ --DATOS INSERTADOS EN CADA TABLA
